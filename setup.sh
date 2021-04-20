8d24f5b1aaa76581aae7cf9eff6d66c8b17db95e
Archit Sharma <77637004+ArchitSharma21@users.noreply.github.com>
2021-04-20 12:46:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"darekarabhishek@gmail.com\"\n\
+email = \"sharchit99k@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
